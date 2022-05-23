b948c4999ed1728b8225f07b352c5dd47831c117
sonsonghyun <asdfghjk99@naver.com>
2022-05-23 15:51:45 +0900

    0523-2

--- src/main/java/com/cos/blog/test/DummyController.java
+++ src/main/java/com/cos/blog/test/DummyController.java
@@ -1,22 +1,77 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+// Rest여서 data를 리턴해주는 controller
 @RestController
 public class DummyController {
 	
 	@Autowired // 의존성 주입 (DI)
 	private UserRepository userRepository;
+	
+	
+	// save함수는 id를 전달하지 않으면 insert를 해주고
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	// save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해준다
+	// <form>태그 데이터가 아닌 json데이터로 받아서
+	// password,email
+	@Transactional // 함수 종료시 자동 commit
+	@PutMapping("/dummy/user/{id}") 
+	public User updateUser(@PathVariable int id,@RequestBody User requestUser) { //json데이터 요청 > java object가 받아줌
+		// 이때 필요한 annotation이 RequestBody
+		System.out.println("id:"+id);
+		System.out.println("password:"+requestUser.getPassword());
+		System.out.println("email:"+requestUser.getEmail());
+		
+		//람다식 () 안에 함수 들어가는거 
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패했습니다");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(user);
+		
+		// 더티체킹 
+		return null;
+	}
+	
+	// http://localhost:8000/blog/dummy/user
+	// 유저 객체 전부 반환
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	// 한페이지당 2건의 데이터를 리턴
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
+		Page<User> pagingUsers=userRepository.findAll(pageable);
+		
+		List<User> users =pagingUsers.getContent();
+		return users;
+	}
+	
+	
 	
 	// {id} 주소로 파라미터를 전달 받을 수 있습니다
 	// http://localhost:8000/blog/dummy/user/3
@@ -32,7 +87,12 @@ public class DummyController {
 				return new IllegalArgumentException("해당 유저는 없습니다. id :"+id);
 			}
 		});
-			
+		// 요청은 웹 브라우져 
+		// user 객체=자바오브젝트 
+		// 변환 > json (Gson 라이브러리)
+		// 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동 
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에 던져준다
 		return user;
 	}
 	
